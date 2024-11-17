dad0c4881989e09c7d07dc2917f75921867e1f0b
010SanЁk010 <119010271+010San010@users.noreply.github.com>
2024-11-17 11:52:12 +0300

    Add files via upload

---
+++ Exercise2_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void Construct(string alfa, string& word, int k) {
+    if (word.size() >= k)
+        return;
+
+    string temp = "\0";
+    for (int i = 0; i < word.size(); i++) {
+        int index = alfa.find(word[i]);
+        temp += alfa[(index + 1) % 26];
+    }
+    word += temp;
+
+    Construct(alfa, word, k);
+}
+
+int main()
+{
+    string alfa = "abcdefghijklmnopqrstuvwxyz";
+    string word;
+    int k;
+
+    cin >> word;
+    cin >> k;
+
+    Construct(alfa, word, k);
+
+    cout << word[k - 1];
+}
+
---
+++ Exercise2_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+bool PowOfThree(int n) {
+    if (n == 1)
+        return true;
+    if (n == 0)
+        return false;
+
+    n /= 3;
+    PowOfThree(n);
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    cout << PowOfThree(n);
+}
